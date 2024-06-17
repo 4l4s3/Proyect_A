a4ac3f3fe925d0c33bec2a09095f66fbf9808eb5
4l4str3 <alastreperez7@gmail.com>
2024-06-17 16:10:23 -0500

    Alertas añadidas; Probadas en Contacto y Venta.

--- AirDc/views.py
+++ AirDc/views.py
@@ -13,6 +13,7 @@ from verify_email.email_handler import send_verification_email
 from .models import Producto
 from rest_framework import viewsets
 from .serializer import productos_serializer
+from django.contrib import messages
 # Create your views here.
 iconF = '''<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
   <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"/>
@@ -43,6 +44,7 @@ def contact(request):
    recipiente = ["us.airdc@gmail.com"]
    send_mail(asunto,mensaje,email,recipiente)
    
+   messages.success(request, "El mensaje ha sido enviado")
   return render(request, 'contact.html',{
         'iconI': mark_safe(iconI),
         'iconF':mark_safe(iconF),
--- carrito/views.py
+++ carrito/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
 from .carrito import Carrito
 from AirDc.models import Producto
+from django.contrib import messages
 
 # Create your views here.
 
@@ -10,7 +11,7 @@ def agregar_producto(request,producto_id):
     producto = Producto.objects.get(id=producto_id)
     
     carro.agregar(producto=producto)
-    
+    messages.success(request, "El producto ha sido agregado al carrito")
     return redirect("/venta/0")
 
 
@@ -20,7 +21,6 @@ def eliminar_producto(request,producto_id):
     producto = Producto.objects.get(id=producto_id)
     
     carro.eliminar(producto=producto)
-    
     return redirect("/venta/0")
 
 
@@ -29,7 +29,7 @@ def restar_producto(request,producto_id):
         
     carro = Carrito(request)
     producto = Producto.objects.get(id=producto_id)
-    
+    messages.success(request, "El producto ha sido eliminado del carrito")
     carro.restar(producto=producto)
     
     return redirect("/venta/0")
